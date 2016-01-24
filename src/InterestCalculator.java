e7c5ee7ca203fbaf2549e06b58e7263cc7f75e6a
John231 <j_b_93@hotmail.co.uk>
2016-01-24 20:49:05 +0000

    Interest calculator
    Basic test class for group Github tutorial

---
+++ src/InterestCalculator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author jonathanbones
+ *	Basic test class for github tutorial
+ */
+public class InterestCalculator {
+	
+		public static void main(String[] arguments){
+			
+			int investment = 9_000;
+			System.out.println("Investment amount is: "+investment);
+			//Calculate the worth after a year of with interest at 6%
+			investment *= 1.06;
+			//Calculate the worth after a year with interest at 8%
+			investment *= 1.08;
+			//Calculate the worth after a year with interest at 10%
+			investment *= 1.1;
+			//Print out the final worth to the display
+			System.out.println("Investment worth after three years is: "+investment);
+		}
+}
